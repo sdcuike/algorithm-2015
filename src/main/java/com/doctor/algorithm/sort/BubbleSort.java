2f967f2fcb944d553903586c09b95eeba800c500
sdcuike <303286730@qq.com>
2015-05-11 00:00:24 +0800

    fix bug

--- src/main/java/com/doctor/algorithm/sort/BubbleSort.java
+++ src/main/java/com/doctor/algorithm/sort/BubbleSort.java
@@ -22,10 +22,10 @@ public class BubbleSort {
 	private static <T> void sort(T[] array, BiPredicate<T, T> biPredicate) {
 		for (int i = array.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
-				if (biPredicate.test(array[j], array[i])) {
+				if (biPredicate.test(array[j], array[j + 1])) {
 					T temp = array[j];
-					array[j] = array[i];
-					array[i] = temp;
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 				}
 			}
 		}
