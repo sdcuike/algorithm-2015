819e3594c65146d7f6bdefec7540775ceae21154
sdcuike <sdcuike@users.noreply.github.com>
2015-06-15 14:41:35 +0800

    add  com.clearspring.analytics.stream

---
+++ src/test/java/com/doctor/javascript/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2014- now() The  algorithm-2015  Authors
+ *
+ * https://github.com/sdcuike
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * javascript 版数据结构练习（node环境，jjs环境）
+ * 
+ * @author doctor
+ *
+ * @time 2015年6月15日 下午2:37:11
+ */
+package com.doctor.javascript;
